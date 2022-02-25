048bf4555e3ca301be7b8fad307fbf3e99cbaa7a
kap-94 <karamjp7@gmail.com>
2022-02-24 19:21:07 -0600

    Project version 2

--- src/js/index.js
+++ src/js/index.js
@@ -30,6 +30,77 @@ $(".brand-slider").jdSlider({
 });
 
 // Navbar Toggle
-$(window).on("scroll", function () {
-  $("nav").toggleClass("scrolled", $(this).scrollTop() > 450);
+// $(window).on("scroll", function () {
+//   $("#navbar-container").toggleClass("scrolled", $(this).scrollTop() > 450);
+// });
+
+// Navbar Sticky
+const nav = document.querySelector("#navbar-container");
+const headingPrimary = document.querySelector(".heading-primary");
+const headingPrimaryHeight = headingPrimary.getBoundingClientRect().height;
+
+const stickyNav = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) nav.classList.add("scrolled");
+  else nav.classList.remove("scrolled");
+};
+
+const headingObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${headingPrimaryHeight}px`,
 });
+
+headingObserver.observe(headingPrimary);
+
+// Reveal Sections
+// const allSections = $(".section");
+const allSections = document.querySelectorAll(".section");
+
+const revealSection = (entries, observer) => {
+  const [entry] = entries;
+
+  if (!entry.isIntersecting) return;
+
+  entry.target.classList.remove("section--hidden");
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach((section) => {
+  sectionObserver.observe(section);
+  section.classList.add("section--hidden");
+});
+
+// Lazy Loading Images
+
+// const imgTargets = document.querySelectorAll("img[data-src]");
+// console.log(imgTargets);
+
+// const loadImg = function (entries, observer) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) return;
+
+//   // Replace src with data-src
+//   entry.target.src = entry.target.dataset.src;
+
+//   entry.target.classList.remove("lazy-img");
+//   entry.target.addEventListener("load", function () {});
+
+//   observer.unobserve(entry.target);
+// };
+
+// const imgObserver = new IntersectionObserver(loadImg, {
+//   root: null,
+//   threshold: 0,
+//   // rootMargin: "200px",
+// });
+
+// imgTargets.forEach((img) => imgObserver.observe(img));
